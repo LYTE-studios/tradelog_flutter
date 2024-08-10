be3efeff14c8e33834dc52825b9e085685dbf9c2
Robin Monseré <51537026+robinmonsere@users.noreply.github.com>
2024-08-10 16:18:17 +0200

    feat: adding sidebarX

---
+++ lib/src/ui/theme/text_styles.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  // --------------------------------------
+  // Title
+  static TextStyle titleLarge = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+    fontSize: 28,
+  );
+
+  static TextStyle titleMedium = TextStyle(
+    fontFamily: 'Inter',
+  );
+
+  static TextStyle titleSmall = TextStyle(
+    fontFamily: 'Inter',
+  );
+
+  // --------------------------------------
+  // body
+  static TextStyle bodyLarge = TextStyle(
+    fontFamily: 'Inter',
+  );
+
+  static TextStyle bodyMedium = TextStyle(
+    fontFamily: 'Inter',
+  );
+
+  static TextStyle bodySmall = TextStyle(
+    fontFamily: 'Inter',
+  );
+
+  // --------------------------------------
+  // label
+  static TextStyle labelLarge = TextStyle(
+    fontFamily: 'Inter',
+  );
+
+  static TextStyle labelMedium = TextStyle(
+    fontFamily: 'Inter',
+  );
+
+  static TextStyle labelSmall = TextStyle(
+    fontFamily: 'Inter',
+  );
+}
---
+++ lib/src/ui/widgets/navigation/tradely_sidebar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class TradelySidebar extends StatelessWidget {
+  const TradelySidebar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
