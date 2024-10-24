615bcd7335e8798eb2a6437862d2575e401099eb
LYTE Studios Manager <lytestudiosglobal@gmail.com>
2024-10-24 16:42:39 +0200

    activity start

---
+++ lib/src/features/dashboard/overview/presentation/containers/activity_heatmap_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
+import 'package:tradelog_flutter/src/ui/base/base_container_expanded.dart';
+
+class ActivityHeatmapContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BaseContainerExpanded();
+  }
+}
