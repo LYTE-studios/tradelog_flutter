98789917896c2dd15d289084126d555dd7f8af86
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-11-14 12:02:10 +0100

    fix: linked accounts

--- lib/src/features/dashboard/account/presentation/widgets/linked_account_block.dart
+++ lib/src/features/dashboard/account/presentation/widgets/linked_account_block.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lyte_studios_flutter_ui/lyte_studios_flutter_ui.dart';
 import 'package:tradelog_client/tradelog_client.dart';
+import 'package:tradelog_flutter/src/core/utils/tradely_number_utils.dart';
 import 'package:tradelog_flutter/src/features/dashboard/account/presentation/widgets/custom_pop_menu.dart';
 import 'package:tradelog_flutter/src/ui/base/base_container.dart';
 import 'package:tradelog_flutter/src/ui/theme/padding_sizes.dart';
@@ -109,7 +110,7 @@ class _LinkedAccountBlockState extends State<LinkedAccountBlock> {
                               height: PaddingSizes.xxs,
                             ),
                             Text(
-                              "\$${totalBalance.toStringAsFixed(2)}",
+                              "\$ ${TradelyNumberUtils.formatValuta(totalBalance)}",
                               style: Theme.of(context)
                                   .textTheme
                                   .bodyLarge
